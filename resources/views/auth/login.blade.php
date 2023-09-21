83abbea91f9ae40472bedc58863005a0a5c0710c
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-09-22 00:11:23 +0200

    add style to login and register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,49 @@
 @extends('layouts.guest')
 
 @section('main-content')
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <label for="email">
-                Email
-            </label>
-            <input type="email" id="email" name="email">
+<h1 class="text-center text-primary text-uppercase">login</h1>
+    <div class="row">
+        <div class="col-6 mx-auto mt-4">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+        
+                <!-- Email Address -->
+                <div class="mb-3">
+                    <label for="email" class="form-label">
+                        Email
+                    </label>
+                    <input type="email" class="form-control" id="email" name="email">
+                </div>
+        
+                <!-- Password -->
+                <div class="mt-4">
+                    <label for="password" class="form-label">
+                        Password
+                    </label>
+                    <input type="password" class="form-control id="password" name="password">
+                </div>
+        
+                <!-- Remember Me -->
+                <div class="block mt-4">
+                    <label for="remember_me">
+                        <input id="remember_me" type="checkbox" name="remember">
+                        <span>Remember me</span>
+                    </label>
+                </div>
+        
+                <div class="d-flex flex-column items-center justify-end mt-4">
+                    @if (Route::has('password.request'))
+                        <a href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+        
+                    <button type="submit" class="btn btn-primary mt-2">
+                        Log in
+                    </button>
+                </div>
+            </form>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <label for="password">
-                Password
-            </label>
-            <input type="password" id="password" name="password">
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me">
-                <input id="remember_me" type="checkbox" name="remember">
-                <span>Remember me</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <button type="submit">
-                Log in
-            </button>
-        </div>
-    </form>
+    </div>
+    
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,49 +1,55 @@
 @extends('layouts.guest')
 
 @section('main-content')
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <label for="name">
-                Name
-            </label>
-            <input type="text" id="name" name="name">
+    <h1 class="text-center text-primary text-uppercase">Register</h1>
+    <div class="row">
+        <div class="col-6 mt-4 mx-auto">
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+        
+                <!-- Name -->
+                <div>
+                    <label class="form-label" for="name">
+                        Name
+                    </label>
+                    <input class="form-control" type="text" id="name" name="name">
+                </div>
+        
+                <!-- Email Address -->
+                <div class="mt-4">
+                    <label class="form-label" for="email">
+                        Email
+                    </label>
+                    <input class="form-control" type="email" id="email" name="email">
+                </div>
+        
+                <!-- Password -->
+                <div class="mt-4">
+                    <label class="form-label" for="password">
+                        Password
+                    </label>
+                    <input class="form-control" type="password" id="password" name="password">
+                </div>
+        
+                <!-- Confirm Password -->
+                <div class="mt-4">
+                    <label class="form-label" for="password_confirmation">
+                        Conferma Password
+                    </label>
+                    <input class="form-control" type="password" id="password_confirmation" name="password_confirmation">
+                </div>
+        
+                <div class="d-flex flex-column items-center justify-end mt-4">
+                    <a href="{{ route('login') }}">
+                        {{ __('Already registered?') }}
+                    </a>
+        
+                    <button type="submit" class="btn btn-primary mt-2">
+                    Register
+                    </button>
+                </div>
+            </form>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <label for="email">
-                Email
-            </label>
-            <input type="email" id="email" name="email">
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <label for="password">
-                Password
-            </label>
-            <input type="password" id="password" name="password">
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <label for="password_confirmation">
-                Conferma Password
-            </label>
-            <input type="password" id="password_confirmation" name="password_confirmation">
-        </div>
-
-        <div>
-            <a href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <button type="submit">
-                Register
-            </button>
-        </div>
-    </form>
+    </div>
+    
 @endsection
